123ac49a373067f7a60a5d5252b1dc73a3c9f7bd
LiuYonghua <yliu19930120@163.com>
2019-03-16 12:41:21 +0800

    增加拉取脚本项目的代码

--- scripts/pullAll.sh
+++ scripts/pullAll.sh
@@ -1,21 +1,41 @@
 #!bin/bash
-projectPath=/home/admin/java/yliu/
+#代码拉取脚本
+
 githost='git@github.com:yliu19930120/'
-projects=('yliu_base_server' 'yliu_crawler' 'yliu_crawler_core' 'yliu_executer' 'yliu_utils_mongo' 'yliu_scheduler' 'yliu_utils_redis')
+javaProPath=/home/admin/java/yliu/
+scriptsPath=/home/admin/
+javaProjects=('yliu_base_server' 'yliu_crawler' 'yliu_crawler_core' 'yliu_executer' 'yliu_utils_mongo' 'yliu_scheduler' 'yliu_utils_redis')
+sciptsProjects=('yliu_shell_scripts')
+
 currentPath=`pwd`
+
 test -d $projectPath || mkdir -p $projectPath
-cd $projectPath
-for p in ${projects[@]}
-do 
-	echo '拉取项目:'${p}
-if [ -e ${p} ]
-then
-	cd ${p}
-	git pull ${githost}${p}
-	cd ..
-else
-	git clone ${githost}${p}
-fi
-done
+
+#拉取函数 参数1:工作路径;参数2:拉取的项目
+function pullOrClone(){
+	
+	projectPat=$1
+	projects=$2
+	
+	echo "进入到路径:$projectPath"
+	cd $projectPath
+	
+	for p in ${projects[@]}
+	do 
+		echo '拉取项目:'${p}
+	if [ -e ${p} ]
+	then
+		cd ${p}
+		git pull ${githost}${p}
+		cd ..
+	else
+		git clone ${githost}${p}
+	fi
+	done
+	}
+
+pullOrClone $javaProPath $javaProjects
+
+pullOrClone $scriptsPath $sciptsProjects
 
 cd $currentPath
