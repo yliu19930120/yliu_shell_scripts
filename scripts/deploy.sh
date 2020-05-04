09cd98349939ffc38297bfb4364bb73e9d9beb5e
LiuYonghua <liuyonghua@ziasset.com>
2020-05-04 09:37:48 +0800

    修改打包脚本

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,19 @@
 #!bin/bash
+baseJars=(yliu_utils_redis yliu_utils_mongo yliu_base_core yliu_base_server yliu_crawler_core)
 declare -A alia=([ec]=yliu_executer [sc]=yliu_scheduler [as]=yliu_appserver)
 workPath=/home/admin/java/work/
 javaPath=/home/admin/code/
-setGradlePath=/home/admin/yliu_shell_scripts/gradle/settings.gradle
+version="1.0-SNAPSHOT"
 
 #接收两个参数1:打包的项目，2:打包的命令
 function package(){
 	echo "------------------------------我是分割线---------------------------------"
 	project=$1	
-	jarPath=${project}/build/libs/${project}-all.jar
+	jarPath=${project}/target/${project}-${version}.jar
 	echo '打包项目:'${project}
-	gradle ${project}:clean ${project}:shadowJar
+	mvn ${project}:clean assembly:assembly
 	echo '复制到工作目录:'
-	cp ${jarPath} ${workPath}/${project}-all.jar -f 
+	cp ${jarPath} ${workPath}/${project}.jar -f
 }
 function guide(){
 	echo "支持以下程序包"
@@ -21,6 +22,18 @@ function guide(){
 	echo ""
 	echo "输入发布项目,多个项目用空格隔开"
 
+}
+
+function packBaseJars(){
+
+  cd ${javaPath}
+  for bj in ${baseJars[@]}
+  do
+    cd ${bj}
+    echo "打包 ${bj}"
+    mvn clean install -DskipTests
+    cd ..
+  done
 }
 function main(){
 	
@@ -33,11 +46,14 @@ function main(){
 	guide
 	read -e name
 	names=`(echo ${name})`
+	pacBase=0
 	for n in ${names[@]}
 	do	
 		project=
 		test -n ${alia[$n]} && project=${alia[$n]}
 		test -z ${project} && echo "ERROR:程序名称错误" && continue
+		test pacBase==0 && packBaseJars && pacBase=1
+
 		cmd=shadowJar
 		test ${project} =~ "appserver" && cmd=build 
 		package ${project}  ${cmd}
@@ -54,7 +70,7 @@ function main(){
 		echo ''
 		echo "启动程序 ${project} 日志输出到${workPath}logs/${project}.log"
 		cd ${workPath}
-		proName=${project}-all.jar
+		proName=${project}.jar
 		test ${project} =~ "appserver" && proName=${project}.jar 
 		nohup java -jar ${proName} >>/dev/null  2>&1 &
 		cd ${javaPath}
